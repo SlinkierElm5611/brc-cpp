8ce9df7801d44b34373593958b4ed77c1f1a7f6d
Stefan Balta <stefan.balta.sb@gmail.com>
2024-03-08 22:32:16 -0500

    1.6 seconds in i9

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <thread>
 
 #define CHAR_BUFFER_SIZE 1000000
-#define THREADS 16
+#define THREADS 32
 
 struct City {
   int sum;
@@ -18,13 +18,14 @@ struct City {
 char get_number_from_char(char c) { return c - '0'; }
 
 void thread_worker(std::unordered_map<std::string, City>& cities,
-                   char *shared_file_buffer, long read_start, long read_end,
+                   long read_start, long read_end,
                    long *compute_start, short thread_id) {
   std::ifstream file("measurements.txt");
   file.seekg(read_start);
   std::streambuf *file_buffer = file.rdbuf();
   char working_city_buffer[100];
   char working_temp_buffer[4];
+	char buffer[CHAR_BUFFER_SIZE];
   short city_counter = 0;
   short temp_counter = 0;
   bool passed_semicolon = false;
@@ -32,12 +33,12 @@ void thread_worker(std::unordered_map<std::string, City>& cities,
   long buffer_index = read_start;
   while (buffer_index < read_end) {
     long stream_size =
-        file_buffer->sgetn(shared_file_buffer + buffer_index, CHAR_BUFFER_SIZE);
-    for (long i = buffer_index; i < buffer_index + stream_size; i++) {
-      if (shared_file_buffer[i] == '\n') {
+        file_buffer->sgetn(buffer, CHAR_BUFFER_SIZE);
+    for (long i = 0; i < stream_size; i++) {
+      if (buffer[i] == '\n') {
         if (!first_newline_found and thread_id > 0) {
           first_newline_found = true;
-          compute_start[thread_id] = i;
+          compute_start[thread_id] = i + buffer_index;
         } else {
           int temp = 0;
           if (temp_counter == 4) {
@@ -81,13 +82,13 @@ void thread_worker(std::unordered_map<std::string, City>& cities,
           city_counter = 0;
           temp_counter = 0;
         }
-      } else if (shared_file_buffer[i] == ';') {
+      } else if (buffer[i] == ';') {
         passed_semicolon = true;
       } else if (!passed_semicolon) {
-        working_city_buffer[city_counter] = shared_file_buffer[i];
+        working_city_buffer[city_counter] = buffer[i];
         city_counter++;
-      } else if (shared_file_buffer[i] != '.') {
-        working_temp_buffer[temp_counter] = shared_file_buffer[i];
+      } else if (buffer[i] != '.') {
+        working_temp_buffer[temp_counter] = buffer[i];
         temp_counter++;
       }
     }
@@ -104,15 +105,13 @@ int main() {
   file.seekg(0, std::ios::end);
   long file_size = file.tellg();
   file.seekg(0, std::ios::beg);
-  char *buffer = new char[file_size];
 	for (int i = 0; i < THREADS; i++) {
-		threads[i] = std::thread(thread_worker, std::ref(cities_threads[i]), buffer, i * (file_size / THREADS), (i + 1) * (file_size / THREADS), compute_start, i);
+		threads[i] = std::thread(thread_worker, std::ref(cities_threads[i]), i * (file_size / THREADS), (i + 1) * (file_size / THREADS), compute_start, i);
 	}
 	for (int i = 0; i < THREADS; i++) {
 		threads[i].join();
 	}
 	std::cout << "Finished Computation and Reading" << std::endl;
-  free(buffer);
 	for (int i = 0; i< THREADS; i++) {
 		std::cout<<cities_threads[i].size()<<std::endl;
 	}
