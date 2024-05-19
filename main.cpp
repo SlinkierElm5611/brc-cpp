fca6949dc07500d9c76b05c026074e76d5abe96a
SlinkierElm5611 <stefan.balta.sb@gmail.com>
2024-05-19 15:58:11 -0400

    Reformated using clang-format

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 #else
 #include "sse2neon.h"
 #endif
+#include <fcntl.h>
 #include <iostream>
 #include <string>
+#include <sys/mman.h>
 #include <thread>
 #include <unordered_map>
-#include <fcntl.h>
-#include <sys/mman.h>
 
 #define MAX_CITY_NAME_SIZE 100
 #define MAX_NUM_KEYS 10000
@@ -29,14 +29,14 @@ struct CityString {
     if (size != other.size) {
       return false;
     }
-#ifdef __x86_64__ 
+#ifdef __x86_64__
     for (int i = 0; i < size / 32; i++) {
       __m256i a = _mm256_loadu_si256((__m256i *)&name[i * 32]);
       __m256i b = _mm256_loadu_si256((__m256i *)&other.name[i * 32]);
       __m256i result = _mm256_cmpeq_epi8(a, b);
       short bit_shift = (size - i * 32 > 32) ? 0 : 32 - size + i * 32;
       int mask = _mm256_movemask_epi8(result) >> bit_shift;
-      if (mask != ((2<<(32-bit_shift)) - 1)) {
+      if (mask != ((2 << (32 - bit_shift)) - 1)) {
         return false;
       }
     }
@@ -47,7 +47,7 @@ struct CityString {
       __m128i result = _mm_cmpeq_epi8(a, b);
       short bit_shift = (size - i * 16 > 16) ? 0 : 16 - size + i * 16;
       int mask = _mm_movemask_epi8(result) >> bit_shift;
-      if (mask != ((2<<(16-bit_shift)) - 1)) {
+      if (mask != ((2 << (16 - bit_shift)) - 1)) {
         return false;
       }
     }
@@ -75,32 +75,28 @@ int get_number_from_chars(const char *c, char size) {
   __m128i operations_vector = _mm_setr_epi32('0', '0', '0', '0');
   numbers = _mm_sub_epi32(numbers, operations_vector);
   if (size == 4) {
-    __m128i result =
-        _mm_mullo_epi32(numbers, _mm_setr_epi32(0, -100, -10, -1));
+    __m128i result = _mm_mullo_epi32(numbers, _mm_setr_epi32(0, -100, -10, -1));
     const int *results = (int *)&result;
     return results[1] + results[2] + results[3];
   } else if (size == 3) {
     if (c[0] == '-') {
-      __m128i result =
-          _mm_mullo_epi32(numbers, _mm_setr_epi32(0, -10, -1, 0));
+      __m128i result = _mm_mullo_epi32(numbers, _mm_setr_epi32(0, -10, -1, 0));
       const int *results = (int *)&result;
       return results[1] + results[2];
     } else {
-      __m128i result =
-          _mm_mullo_epi32(numbers, _mm_setr_epi32(100, 10, 1, 0));
+      __m128i result = _mm_mullo_epi32(numbers, _mm_setr_epi32(100, 10, 1, 0));
       const int *results = (int *)&result;
       return results[0] + results[1] + results[2];
     }
   }
-  __m128i result =
-      _mm_mullo_epi32(numbers, _mm_setr_epi32(10, 1, 0, 0));
+  __m128i result = _mm_mullo_epi32(numbers, _mm_setr_epi32(10, 1, 0, 0));
   const int *results = (int *)&result;
   return results[0] + results[1];
 }
 
 void thread_worker(
     std::unordered_map<CityString, City, CityStringHashFn> &cities,
-    long read_start, long *compute_end, short thread_id, char* buffer) {
+    long read_start, long *compute_end, short thread_id, char *buffer) {
   char working_city_buffer[MAX_CITY_NAME_SIZE];
   char working_temp_buffer[4];
   char city_counter = 0;
@@ -109,51 +105,51 @@ void thread_worker(
   bool first_newline_found = false;
   long buffer_index = read_start;
   while (buffer_index < compute_end[thread_id]) {
-		if (buffer[buffer_index] == '\n') {
-			if (!first_newline_found and thread_id > 0) {
-				first_newline_found = true;
-				compute_end[thread_id - 1] = buffer_index;
-			} else {
-				int temp = get_number_from_chars(working_temp_buffer, temp_counter);
-				passed_semicolon = false;
-				CityString city_name;
-				city_name.size = city_counter;
-				for (int j = 0; j < city_counter; j++) {
-					city_name.name[j] = working_city_buffer[j];
-				}
-				auto city = cities.find(city_name);
-				if (city == cities.end()) {
-					City new_city;
-					new_city.sum = temp;
-					new_city.count = 1;
-					new_city.max = temp;
-					new_city.min = temp;
-					cities[city_name] = new_city;
-				} else {
-					City *found_city = &city->second;
-					found_city->sum += temp;
-					found_city->count++;
-					if (temp > found_city->max) {
-						found_city->max = temp;
-					}
-					if (temp < found_city->min) {
-						found_city->min = temp;
-					}
-				}
-			}
-			city_counter = 0;
-			temp_counter = 0;
-			passed_semicolon = false;
-		} else if (buffer[buffer_index] == ';') {
-			passed_semicolon = true;
-		} else if (!passed_semicolon) {
-			working_city_buffer[city_counter] = buffer[buffer_index];
-			city_counter++;
-		} else if (buffer[buffer_index] != '.') {
-			working_temp_buffer[temp_counter] = buffer[buffer_index];
-			temp_counter++;
-		}
-    buffer_index ++;
+    if (buffer[buffer_index] == '\n') {
+      if (!first_newline_found and thread_id > 0) {
+        first_newline_found = true;
+        compute_end[thread_id - 1] = buffer_index;
+      } else {
+        int temp = get_number_from_chars(working_temp_buffer, temp_counter);
+        passed_semicolon = false;
+        CityString city_name;
+        city_name.size = city_counter;
+        for (int j = 0; j < city_counter; j++) {
+          city_name.name[j] = working_city_buffer[j];
+        }
+        auto city = cities.find(city_name);
+        if (city == cities.end()) {
+          City new_city;
+          new_city.sum = temp;
+          new_city.count = 1;
+          new_city.max = temp;
+          new_city.min = temp;
+          cities[city_name] = new_city;
+        } else {
+          City *found_city = &city->second;
+          found_city->sum += temp;
+          found_city->count++;
+          if (temp > found_city->max) {
+            found_city->max = temp;
+          }
+          if (temp < found_city->min) {
+            found_city->min = temp;
+          }
+        }
+      }
+      city_counter = 0;
+      temp_counter = 0;
+      passed_semicolon = false;
+    } else if (buffer[buffer_index] == ';') {
+      passed_semicolon = true;
+    } else if (!passed_semicolon) {
+      working_city_buffer[city_counter] = buffer[buffer_index];
+      city_counter++;
+    } else if (buffer[buffer_index] != '.') {
+      working_temp_buffer[temp_counter] = buffer[buffer_index];
+      temp_counter++;
+    }
+    buffer_index++;
   }
 }
 
@@ -165,15 +161,16 @@ int main() {
     cities_threads[i].reserve(MAX_NUM_KEYS);
   }
   std::thread *threads = new std::thread[THREADS];
-	int fd = open("measurements.txt", O_RDONLY);
-	uint64_t file_size = lseek(fd, 0L, SEEK_END);
-	void* data_ptr = mmap(nullptr, file_size, PROT_READ, MAP_SHARED,fd, 0);
-	char* char_ptr = reinterpret_cast<char*>(data_ptr);
+  int fd = open("measurements.txt", O_RDONLY);
+  uint64_t file_size = lseek(fd, 0L, SEEK_END);
+  void *data_ptr = mmap(nullptr, file_size, PROT_READ, MAP_SHARED, fd, 0);
+  char *char_ptr = reinterpret_cast<char *>(data_ptr);
   long *compute_end = new long[THREADS];
   for (int i = 0; i < THREADS; i++) {
     compute_end[i] = (i + 1) * (file_size / THREADS);
-    threads[i] = std::thread(thread_worker, std::ref(cities_threads[i]),
-                             i * (file_size / THREADS), compute_end, i, char_ptr);
+    threads[i] =
+        std::thread(thread_worker, std::ref(cities_threads[i]),
+                    i * (file_size / THREADS), compute_end, i, char_ptr);
   }
   for (int i = 0; i < THREADS; i++) {
     threads[i].join();
@@ -200,7 +197,7 @@ int main() {
   std::cout << "Finished Merging" << std::endl;
   delete[] threads;
   delete[] compute_end;
-	munmap(data_ptr, file_size);
-	close(fd);
+  munmap(data_ptr, file_size);
+  close(fd);
   return 0;
 }
